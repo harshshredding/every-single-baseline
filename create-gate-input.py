42e66360e794d086ba521e86e3151b2f8059f690
Nadia Bilal <786.nadya@gmail.com>
2022-06-20 12:34:33 -0400

    add utility to create input files for gate

---
+++ create-gate-input.py
@@ -0,0 +1,21 @@
+import os
+import csv
+from train_annos import get_annos_dict
+tweet_to_annos = get_annos_dict()
+print(len(tweet_to_annos))
+train_folder_path = '/home/claclab/harsh/smm4h-2022-social-dis-ner/socialdisner-data/train-valid-txt-files/training'
+for file_index in range(5):
+    with open(f'gate_input_{file_index+1}.csv', 'w') as csv_output:
+        writer = csv.writer(csv_output)
+        header = ['twitter_id', 'tweet_text', 'spans']
+        writer.writerow(header)
+        data_files_list = os.listdir(train_folder_path)[file_index*1000:(file_index+1)*1000]
+        for filename in data_files_list:
+            data_file_path = os.path.join(train_folder_path, filename)
+            with open(data_file_path, 'r') as f:
+                data = f.read()
+            twitter_id = filename[:-4]
+            tweet_annos = tweet_to_annos.get(twitter_id, [])
+            spans = "@".join([f"{anno['begin']}:{anno['end']}" for anno in tweet_annos])
+            row_to_write = [twitter_id, data, spans]
+            writer.writerow(row_to_write)
---
+++ train_annos.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+def get_annos_dict():
+    annos_file_path = '/home/claclab/harsh/smm4h-2022-social-dis-ner/socialdisner-data/mentions.tsv'
+    df = pd.read_csv(annos_file_path, sep='\t')
+    tweet_to_annos = {}
+    for i, row in df.iterrows():
+        annos_list = tweet_to_annos.get(str(row['tweets_id']), [])
+        annos_list.append({"begin": row['begin'], "end": row['end'], "extraction": row['extraction']})
+        tweet_to_annos[str(row['tweets_id'])] = annos_list
+    return tweet_to_annos
