e90a218017d6ebd8587c2fd83570fdd81529ab11
Harsh Verma <h_ver@viryasubmit.encs.concordia.ca>
2022-07-02 16:04:42 -0400

    use the spanish bert model

--- models.py
+++ models.py
@@ -3,13 +3,14 @@ import torch.nn as nn
 import torch
 from mi_rim import *
 import pytorch_lightning as pl
+from args import args
 
 class SeqLabeler(torch.nn.Module):
     def __init__(self, num_mechanisms, hidden_size, top_k, num_class):
         super(SeqLabeler, self).__init__()
         self.num_class = num_class
         self.rim = MI_RIM('lstm', num_mechanisms, top_k, hidden_size)
-        self.bert_model = AutoModel.from_pretrained('bert-base-uncased')
+        self.bert_model = AutoModel.from_pretrained(args['bert_model_name'])
         self.classifier = nn.Linear(128, 2)
 
     def forward(self, encoding):
@@ -27,7 +28,7 @@ class LITSeqLabeler(pl.LightningModule):
         super(LITSeqLabeler, self).__init__()
         self.num_class = num_class
         self.rim = MI_RIM('lstm', num_mechanisms, top_k, hidden_size)
-        self.bert_model = AutoModel.from_pretrained('bert-base-uncased')
+        self.bert_model = AutoModel.from_pretrained(args['bert_model_name'])
         self.classifier = nn.Linear(128, self.num_class)
 
     def forward(self, encoding):
