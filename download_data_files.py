12c5968fb0f805d9eeabd67d410247f3817962a3
Harsh Verma <h_ver@encs.concordia.ca>
2022-07-10 15:21:53 -0400

    script to download data files

---
+++ download_data_files.py
@@ -0,0 +1,14 @@
+import gdown
+
+url = "https://drive.google.com/file/d/1OQuQ4qxTB8fPeLfEoGujUkiin2Tg1isU/view?usp=sharing"
+output = "gate_output.zip"
+gdown.download(url=url, output=output, quiet=False, fuzzy=True)
+
+url = "https://drive.google.com/file/d/19rZltDngthsoEgY5DSwIaviWyX5xPV8s/view?usp=sharing"
+output = "data_files.zip"
+gdown.download(url=url, output=output, quiet=False, fuzzy=True)
+
+url = "https://drive.google.com/file/d/18bS-b1sXyiKV7wntyA7439UoxOIQeuJx/view?usp=sharing"
+output = "embeddings.zip"
+gdown.download(url=url, output=output, quiet=False, fuzzy=True)
+
