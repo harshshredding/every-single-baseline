07ebec46f75ffc7bca1559a1691ba14995499406
Harsh Verma <harshshredding@gmail.com>
2022-07-14 21:02:36 +0000

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+git clone https://github.com/harshshredding/smm4h-social-dis-ner.git
+cd smm4h-social-dis-ner/
+pip install -r requirements.txt
+apt-get install vim
+git config --global user.email "harshshredding@gmail.com"
+git config --global user.name "Harsh Verma"
