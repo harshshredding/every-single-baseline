042e0138956f349ae0f8eace5a374e84b12bf1f0
Harsh Verma <h_ver@encs.concordia.ca>
2022-07-17 13:40:27 -0400

    adamw experiment

--- args.py
+++ args.py
@@ -7,8 +7,8 @@ args = {
     "training_data_folder_path": "./gate-output-new-matching/train",
     "validation_data_folder_path": "./gate-output-new-matching/valid",
     "test_data_folder_path": "./gate-output-test",
-    "bert_model_name": "dccuchile/bert-base-spanish-wwm-cased",
-    #"bert_model_name": "pysentimiento/robertuito-base-uncased",
+    # "bert_model_name": "dccuchile/bert-base-spanish-wwm-cased",
+    "bert_model_name": "xlm-roberta-large",
     "bert_model_output_dim": 1024,
     "num_epochs": 15,
     "save_models_dir": "./models",
@@ -17,11 +17,11 @@ args = {
     "raw_test_files_path": "./test_data/test-data/test-data-txt-files",
     "umls_embeddings_path": "./embeddings.csv",
     "testing_mode": True,
-    "experiment_name": "ranger_pos_encod",
+    "experiment_name": "AdamW_roberta_large",
     "pos_embeddings_path": './spanish_pos_emb.p',
     "disease_gazetteer_path": './dictionary_distemist.tsv',
     "errors_dir": './errors',
-    "model_name": "PosEncod3ClassesNoSilverSpanish",
-    "optimizer": "Adam"
+    "model_name": "PosEncod3ClassesNoSilverNewGaz",
+    "optimizer": "AdamW"
 }
 default_key = "DEFAULT"
