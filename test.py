d48194bc1a426c0120d1bba6d2d184991ed9907d
Harsh Verma <harshshredding@gmail.com>
2022-11-22 08:48:44 -0500

    fix bug where empty batch encoding would cause training to fail

--- test.py
+++ test.py
@@ -3,45 +3,18 @@ import util
 import spacy
 from structs import Sample, Anno
 from typing import List
+from transformers import AutoTokenizer
+from args import args
 
-nlp = spacy.load("en_core_web_sm")
-print(type(nlp))
-sample_text = "This is a disease. This is another disease. This disease is awesome."
-sample_id = "some_id"
-sample_annos = [Anno(10,17,'label','disease'), Anno(35, 42, 'label', 'disease'), Anno(49, 56, 'label','disease')]
-test_sample = Sample(sample_text, sample_id, sample_annos)
-# spacy_doc = nlp(some_text)
-# assert spacy_doc.has_annotation("SENT_START")
-# start_annos = [(10,17), (35, 42), (49, 56)]
-# end_annos = [(10,17), (16, 23), (5, 12)]
-# for i, sent in enumerate(spacy_doc.sents):
-#     print(some_text[sent.start_char: sent.end_char])
-#     sent_start_offset = sent.start_char
-#     print((start_annos[i][0] - sent_start_offset,start_annos[i][1] - sent_start_offset))
-
-def make_sentence_samples(sample: Sample) -> List[Sample]:
-    """
-    Takes a sample and creates mini-samples, where each
-    mini-sample represents a sentence.
-    """
-    ret_sent_samples = []
-    spacy_doc = nlp(sample.text)
-    assert spacy_doc.has_annotation("SENT_START")
-    for i, sent in enumerate(spacy_doc.sents):
-        annos_contained_in_sent = [anno for anno in sample.annos if (sent.start_char <= anno.begin_offset and anno.end_offset <= sent.end_char)]
-        sent_annos = []
-        for contained_anno in annos_contained_in_sent:
-            new_start = contained_anno.begin_offset - sent.start_char
-            new_end = contained_anno.end_offset - sent.start_char
-            new_extraction = sent.text[new_start:new_end]
-            sent_annos.append(Anno(new_start, new_end, contained_anno.label_type, new_extraction))
-        ret_sent_samples.append(Sample(sent.text, f"{sample.id}_sent_{i}", sent_annos)) 
-    return ret_sent_samples
-
-
-print(make_sentence_samples(test_sample))
-
-
+train_texts = train_util.get_train_texts()
+train_tokens = train_util.get_train_tokens()
+bert_tokenizer = AutoTokenizer.from_pretrained(args['bert_model_name'])
+text = train_texts['caso_clinico_neurologia185_sent_59']
+tokens = train_tokens['caso_clinico_neurologia185_sent_59']
+print(f"text: {text}, tokens: {tokens}")
+batch_encoding = bert_tokenizer(util.get_token_strings(tokens), return_tensors="pt", is_split_into_words=True,
+                                        add_special_tokens=False, truncation=True, max_length=512)
+print(len(batch_encoding.words()))
 # for token in valid_tokens['cc_odontologia25']:
 #     print(token.token_start_offset, token.token_end_offset, token.token_string)
 # for anno in valid_annos['cc_odontologia25']:
--- train.py
+++ train.py
@@ -62,6 +62,8 @@ for epoch in range(args['num_epochs']):
         tokens = util.get_token_strings(sample_data)
         batch_encoding = bert_tokenizer(tokens, return_tensors="pt", is_split_into_words=True,
                                         add_special_tokens=False, truncation=True, max_length=512).to(device)
+        if not len(batch_encoding.word_ids()): # If we don't have any tokens, no point training
+            continue
         expanded_labels = train_util.extract_expanded_labels(sample_data, batch_encoding, annos)
         expanded_labels_indices = [label_to_idx_dict[label] for label in expanded_labels]
         model_input = train_util.prepare_model_input(batch_encoding, sample_data)
