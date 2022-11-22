ca7ed039d7420bd164d70570b35bcbf0c95c19e1
Harsh Verma <harshshredding@gmail.com>
2022-11-22 17:21:30 -0500

    constituency test

--- test.py
+++ test.py
@@ -1,13 +1,18 @@
-from typing import List
-
-import stanza
-from transformers import AutoTokenizer
-
-from structs import Anno, Sample
-
-stanza.download('en')
-nlp = stanza.Pipeline('en', processors='tokenize,pos,constituency')
+import benepar
+benepar.download('benepar_en3')
+import spacy
+from spacy.tokens.span import Span
+nlp = spacy.load('en_core_web_md')
+if spacy.__version__.startswith('2'):
+    nlp.add_pipe(benepar.BeneparComponent("benepar_en3"))
+else:
+    nlp.add_pipe("benepar", config={"model": "benepar_en3"})
 doc = nlp("he rode a scott motorcycle winning several trophies")
-for sentence in doc.sentences:
-    print(sentence.constituency)
-
+sent = list(doc.sents)[0]
+print("\n"*3)
+print(sent._.parse_string)
+print("\n"*3)
+# print(type(sent._))
+# second_part : Span = list(sent._.children)[1]
+# print(second_part.start_char, second_part.end_char)
+# print(len("he rode a scott motorcycle winning several trophies"))
