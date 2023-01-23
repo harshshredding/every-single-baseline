6c61270ac48f59fd6c2c0fe8ac33934a9cec8423
Harsh Verma <harshshredding@gmail.com>
2023-01-23 14:10:18 -0800

    annotate noun phrases

--- report.md
+++ report.md
@@ -1,8 +1,23 @@
-### Table of contents
-- [Bert Linear SeqLabel](#bert-linear-seqlabel)
-- [SpanNER](#spanner)
-- [Bert Seq Label CRF](#bert-seq-label-crf)
+### Models
+- Bert Linear Sequence Labeling
+    - Bert Layer representing tokens.
+    - Linear Classifier Layer classifying each token to BIO labels.
+- Bert Seq Label CRF
+    - Bert Layer representing tokens.
+    - A CRF Classifier Layer classiying each token to BIO labels. 
+- [SpanNER](https://arxiv.org/abs/2106.00641)
+    - Bert Layer representing tokens.
+    - Layer that represents all possible spans using the beginning and ending token representations.
+    - Linear Classifier Layer classifying each token to BIO labels.
 
-### Bert Linear SeqLabel
-### SpanNER
-### Bert Seq Label CRF
+### Errors
+#### Multiconer Fine
+- Cannot tell difference between PublicCorp and PrivateCorp.
+- Definitely needs external knowledge.
+- False Positives:
+  > the adoption of the ( of charles v ) in 1532 made **inquisitional** procedures empirical law.  
+  
+  lacks syntax knowledge.
+  > **it** was written by tom johnston .
+
+  Respectable mistake.
--- test.py
+++ test.py
@@ -1,15 +1,43 @@
-from utils.config import get_model_config_by_name, get_dataset_config_by_name
-from utils.universal import *
-from models import JustBert3ClassesCRF
-import util
-import train_util
+import benepar, spacy
+from spacy.tokens.span import Span
+from typing import List
+from structs import Anno
 
-model_config = get_model_config_by_name('JustBert3Classes')
-dataset_config = get_dataset_config_by_name('multiconer_coarse')
-all_types = util.get_all_types(dataset_config.types_file_path, dataset_config.num_types)
-token_data_dict = train_util.get_valid_token_data_dict(dataset_config)
-annos_dict = train_util.get_valid_annos_dict(dataset_config)
-sample_token_data = token_data_dict['5239d808-f300-46ea-aa3b-5093040213a3']
-sample_annos = annos_dict['5239d808-f300-46ea-aa3b-5093040213a3']
-model = JustBert3ClassesCRF(all_types, model_config, dataset_config)
-model.forward(sample_token_data, sample_annos, model_config)
+def get_all_noun_phrases(span) -> List[Span]:
+    ret = []
+    child: Span
+    for child in span._.children:
+        child_labels = child._.labels 
+        if 'NP' in child_labels:
+            ret.append(child)
+        ret.extend(get_all_noun_phrases(child))
+    return ret
+
+def get_noun_phrase_annos(span) -> List[Anno]:
+    noun_phrases = get_all_noun_phrases(span)
+    noun_phrase: Span
+    return [Anno(noun_phrase.start_char, noun_phrase.end_char, 'NounPhrase', str(noun_phrase), {}) 
+            for noun_phrase in noun_phrases]
+
+def print_sentence_info(sentence):
+    print(sentence._.parse_string)
+    print()
+    all_noun_phrases = get_all_noun_phrases(sentence)
+    for noun_phrase in all_noun_phrases:
+        print(noun_phrase)
+    print("-"*10)
+
+benepar.download('benepar_en3')
+nlp = spacy.load('en_core_web_md')
+nlp.add_pipe('benepar', config={'model': 'benepar_en3'})
+
+doc = nlp("This is a sentence."\
+          "And this is another sentence."\
+          "I love apples and bananas."\
+          "John loves Alice"\
+          )
+
+for sent in doc.sents:
+    print_sentence_info(sent)
+    annos = get_noun_phrase_annos(sent)
+    print(annos)
