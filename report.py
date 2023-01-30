ac73f23a368ef664e5493f3244fe54ce9d5012cd
Harsh Verma <harshshredding@gmail.com>
2023-01-30 09:02:10 -0800

    update report.py

--- report.py
+++ report.py
@@ -6,11 +6,12 @@ import glob
 
 def download_all_performance_files():
     util.create_directory_structure('./downloaded_results')
-
-    all_files = dropbox.get_all_file_names_in_folder()
-    performance_files = [file_name for file_name in all_files if 'performance' in file_name]
-    semeval_files = [file_name for file_name in performance_files if 'semeval' in file_name]
-
+    semeval_files = [
+        'performance_span_noun_phrase_15_epochs.csv',
+        'performance_seq_label_semeval.csv',
+        'performance_semeval_spanner.csv',
+        'performance_crf_seq_label_semeval.csv'
+    ]
     for semeval_performance_file in semeval_files:
         dropbox_path = f'/{semeval_performance_file}'
         local_download_path = f'./downloaded_results/{semeval_performance_file}'
@@ -27,7 +28,7 @@ def main():
                 ExperimentResult(Experiment(experiment.experiment_name, experiment.dataset_name), score)
             )
     all_datasets = ['legaleval_judgement', 'legaleval_preamble', 'multiconer_coarse', 'multiconer_fine']
-    column_names = ['dataset_name', 'seq_label_semeval', 'semeval_spanner', 'crf_seq_label_semeval']
+    column_names = ['dataset_name', 'seq_label_semeval', 'semeval_spanner', 'crf_seq_label_semeval', 'span_noun_phrase_15_epochs']
     table_data = []
     for dataset_name in all_datasets:
         row = [dataset_name]
@@ -36,7 +37,7 @@ def main():
                 if experiment_result.experiment.experiment_name == experiment_name \
                         and experiment_result.experiment.dataset_name == dataset_name:
                     row.append(experiment_result.max_performance)
-        assert len(row) == 4
+        assert len(row) == 5
         table_data.append(row)
     generate_table_pdf(table_data, column_names, './table.pdf')
 
