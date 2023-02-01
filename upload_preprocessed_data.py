e28095b109c9e36a1b7e8f3c6fdc289723f49483
Harsh Verma <harshshredding@gmail.com>
2023-01-31 19:00:33 -0500

    delete previous zip file before uploading

--- upload_preprocessed_data.py
+++ upload_preprocessed_data.py
@@ -1,8 +1,10 @@
+import util
 import utils.dropbox as dropbox_util
 import subprocess
 from preamble import *
 
+util.remove_if_exists('./preprocessed_data.zip')
 # zip preprocessed data
-subprocess.run(['zip','-r','preprocessed_data.zip','preprocessed_data'])
+subprocess.run(['zip', '-r', 'preprocessed_data.zip', 'preprocessed_data'])
 # upload zipped file to dropbox
 dropbox_util.upload_big_file_cleaner('./preprocessed_data.zip', '/preprocessed_data.zip')
