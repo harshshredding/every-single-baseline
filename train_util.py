d769d8c003bc57bd240865137fa6db28c104dce2
Harsh Verma <harshshredding@gmail.com>
2023-02-07 14:06:14 -0500

    printing item lists better

--- train_util.py
+++ train_util.py
@@ -17,7 +17,7 @@ def get_user_input(input_message: str, possible_values: List[str]):
     options_string = ''
     for option in possible_values:
         options_string += f"- {option}\n"
-    user_input = input(f"{input_message}\n choose from: \n {options_string}")
+    user_input = input(f"{input_message}\n Choose from: \n{options_string}")
     if len(possible_values):
         while user_input not in possible_values:
             user_input = input(f"incorrect input '{user_input}', please choose from the given possible values: \n")
