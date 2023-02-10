4607cdb9450ada26cbdf0f710b886fb95c43eb49
Harsh Verma <harshshredding@gmail.com>
2023-02-10 00:03:24 -0500

    get ready for the batched biomedical base experiment

---
+++ experiments/batched_biomedical_base.py
@@ -0,0 +1,7 @@
+from utils.config import get_experiment_config
+
+experiments = [
+    get_experiment_config(model_config_name='BatchedBertSpanish', dataset_name='social_dis_ner'),
+    get_experiment_config(model_config_name='BatchedBertSpanish', dataset_name='living_ner'),
+    get_experiment_config(model_config_name='BatchedBert', dataset_name='genia'),
+]
--- experiments/batched_test.py
+++
@@ -1,7 +0,0 @@
-from utils.config import get_experiment_config
-
-experiments = [
-    get_experiment_config(model_config_name='BatchedBert', dataset_name='social_dis_ner'),
-    get_experiment_config(model_config_name='BatchedBert', dataset_name='living_ner'),
-    get_experiment_config(model_config_name='BatchedBert', dataset_name='genia'),
-]
