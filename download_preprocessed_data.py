af8b31d9a71009a4cda3f4ca22ff45f4559b7856
Harsh Verma <harshshredding@gmail.com>
2023-02-22 13:39:57 -0500

    unzip preprocessed data after downloading it

--- download_preprocessed_data.py
+++ download_preprocessed_data.py
@@ -1,7 +1,13 @@
 import utils.dropbox as dropbox_util
 import util
 import os
+import zipfile
 
+print("downloading preprocessed data")
 if os.path.isdir('./preprocessed_data'):
     util.delete_preprocessed_data_folder()
 dropbox_util.download_file('/preprocessed_data.zip', './preprocessed_data.zip')
+
+print("unzipping downloaded file")
+with zipfile.ZipFile('preprocessed_data.zip', 'r') as zip_ref:
+    zip_ref.extractall('./')
