dab3eaa1a727480927a44688d8ee5bde57f78f16
Harsh Verma <harshshredding@gmail.com>
2023-03-21 19:49:56 -0400

    retract

--- experiments/experiment_compare_span_old_and_new.py
+++ experiments/experiment_compare_span_old_and_new.py
@@ -9,11 +9,4 @@ experiments = [
         model_config_module_name='model_span_large_custom_tokenization_no_batch',
         dataset_config_name='multiconer_fine_tokens'
     ),
-    get_experiment_config(
-        model_config_module_name='model_span_large_no_special_tokens',
-        dataset_config_name='multiconer_fine_vanilla'
-    ),
 ]
-
-# the last experiment should run with a batch size of 4
-experiments[-1].model_config.batch_size = 4
