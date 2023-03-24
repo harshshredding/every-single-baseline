0ab2fec515d1b987465b12f52c0909c961fcd119
Harsh Verma <harshshredding@gmail.com>
2023-03-23 22:13:51 -0400

    forgot to rename model config

--- configs/model_configs/model_seq_base_custom_tokens.py
+++ configs/model_configs/model_seq_base_custom_tokens.py
@@ -1,6 +1,6 @@
 from utils.config import ModelConfig, get_small_model_config
 
 model_config: ModelConfig = get_small_model_config(
-    model_config_name='seq_large_custom_tokenization',
+    model_config_name='seq_base_custom_tokenization',
     model_name='SeqLabelerBatched'
 )
