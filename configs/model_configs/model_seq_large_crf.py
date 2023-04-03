0d5e9020b0fa428a9fd1e1d25aa0e203a9c10d35
Harsh Verma <harshshredding@gmail.com>
2023-04-02 20:43:37 -0400

    use new way of making model configs

--- configs/model_configs/model_seq_large_crf.py
+++ configs/model_configs/model_seq_large_crf.py
@@ -1,8 +1,8 @@
 from utils.config import ModelConfig, get_large_model_config
 
-def create_model_config():
+def create_model_config(model_config_module_name):
     model_config: ModelConfig = get_large_model_config(
-        model_config_name='model_seq_large_default_crf',
+        model_config_name=model_config_module_name,
         model_name='SeqLabelerDefaultCRF'
     )
 
