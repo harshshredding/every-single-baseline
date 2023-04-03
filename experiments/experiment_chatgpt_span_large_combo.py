828efaa490b465b09f80323274e7afad89c00baf
Harsh Verma <harshshredding@gmail.com>
2023-04-03 18:19:14 -0400

    make experiment richer

--- experiments/experiment_chatgpt_span_large_combo.py
+++ experiments/experiment_chatgpt_span_large_combo.py
@@ -5,7 +5,16 @@ experiments = [
         model_config_module_name='model_span_large_default',
         dataset_config_name='social_dis_ner_chatgpt_combo'
     ),
+    get_experiment_config(
+        model_config_module_name='model_seq_large_default',
+        dataset_config_name='social_dis_ner_chatgpt_combo'
+    ),
+    get_experiment_config(
+        model_config_module_name='model_seq_large_crf',
+        dataset_config_name='social_dis_ner_chatgpt_combo'
+    ),
 ]
 
 for experiment in experiments:
     experiment.model_config.num_epochs = 20
+
