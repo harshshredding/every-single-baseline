639622460e96fd27e922ed813c373c0aba61fca3
Harsh Verma <harshshredding@gmail.com>
2023-04-04 19:58:29 -0400

    try experiment with less span width

---
+++ experiments/experiment_living_ner_window_span_fixed_width.py
@@ -0,0 +1,11 @@
+from utils.config import get_experiment_config
+
+experiments = [
+    get_experiment_config(
+        model_config_module_name='model_span_large_default',
+        dataset_config_name='living_ner_window'
+    ),
+]
+
+experiments[0].model_config.num_epochs = 20
+experiments[0].model_config.max_span_length = 32
