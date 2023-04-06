fd25d0dc9d4dce191c64f1485cea000dc2d1697d
Harsh Verma <harshshredding@gmail.com>
2023-04-05 20:10:30 -0400

    fix model config

--- experiments/experiment_social_dis_ner_seq_external_chatgpt_features.py
+++ experiments/experiment_social_dis_ner_seq_external_chatgpt_features.py
@@ -2,7 +2,7 @@ from utils.config import get_experiment_config
 
 experiments = [
     get_experiment_config(
-        model_config_module_name='model_seq_label_default_external',
+        model_config_module_name='model_seq_large_external',
         dataset_config_name='social_dis_ner_external_chatgpt'
     ),
 ]
