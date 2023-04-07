b5eabc2b66d29bf3b9be3a2bcfd5cba5f74d5958
Harsh Verma <harshshredding@gmail.com>
2023-04-07 12:41:31 -0400

    try not relying on super for endpoint span extractor

--- models/span_default_external.py
+++ models/span_default_external.py
@@ -7,9 +7,10 @@ from structs import Anno, Sample
 from transformers.tokenization_utils_base import BatchEncoding
 import torch
 from preamble import *
-from utils.model import PositionalEncodingOriginal, EndpointSpanExtractor
+from utils.model import PositionalEncodingOriginal
 from models.seq_label_default_external import get_bert_embeddings_with_external_knowledge_for_batch,\
         get_bert_embeddings_with_external_knowledge_pos_for_batch
+from allennlp.modules.span_extractors.endpoint_span_extractor import EndpointSpanExtractor
 
 
 class SpanDefaultExternal(SpanDefault):
@@ -20,10 +21,7 @@ class SpanDefaultExternal(SpanDefault):
         assert model_config.external_feature_type is not None
         self.external_feature_type = model_config.external_feature_type
         self.classifier = nn.Linear(2*(self.input_dim + 2), self.num_class)
-
-
-    def get_endpoint_span_extractor(self):
-        return EndpointSpanExtractor(self.input_dim + 2)
+        self.endpoint_span_extractor = EndpointSpanExtractor(self.input_dim + 2)
 
 
     def get_bert_embeddings_for_batch(self, encoding: BatchEncoding, samples: list[Sample]):
@@ -35,6 +33,7 @@ class SpanDefaultExternal(SpanDefault):
                 input_dim=self.input_dim,
                 transformer=self.transformer
         )
+
 
 class SpanDefaultExternalPos(SpanDefaultExternal):
     def __init__(self, all_types: List[str], model_config: ModelConfig, dataset_config: DatasetConfig):
