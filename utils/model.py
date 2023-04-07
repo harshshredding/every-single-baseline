9e73c1ed3634b743fcfdff8c5885a4aeca04c942
Harsh Verma <harshshredding@gmail.com>
2023-04-07 12:29:54 -0400

    use custom endpoint span extractor

--- utils/model.py
+++ utils/model.py
@@ -3,11 +3,22 @@ import torch
 import math
 from torch import Tensor
 from transformers.tokenization_utils_base import BatchEncoding
+from typing import Optional
 
 from structs import Anno, Sample
 from typing import List
 from abc import ABC, abstractmethod
 from utils.config import ModelConfig, DatasetConfig
+
+import torch
+from torch.nn.parameter import Parameter
+
+from allennlp.modules.span_extractors.span_extractor import SpanExtractor
+from allennlp.modules.span_extractors.span_extractor_with_span_width_embedding import (
+    SpanExtractorWithSpanWidthEmbedding,
+)
+from allennlp.nn import util
+from allennlp.common.checks import ConfigurationError
 
 PredictionsBatch = List[List[Anno]]
 
@@ -109,3 +120,259 @@ def get_bert_embeddings_for_batch(bert_model, encoding: BatchEncoding):
     # SHAPE: (batch, seq, emb_dim)
     bert_embeddings_batch = bert_embeddings_batch['last_hidden_state']
     return bert_embeddings_batch
+
+
+def get_device_of(tensor: torch.Tensor) -> int:
+    """
+    Returns the device of the tensor.
+    """
+    if not tensor.is_cuda:
+        return -1
+    else:
+        return tensor.get_device()
+
+
+def get_range_vector(size: int, device: int) -> torch.Tensor:
+    """
+    Returns a range vector with the desired size, starting at 0. The CUDA implementation
+    is meant to avoid copy data from CPU to GPU.
+    """
+    if device > -1:
+        return torch.cuda.LongTensor(size, device=device).fill_(1).cumsum(0) - 1
+    else:
+        return torch.arange(0, size, dtype=torch.long)
+
+
+def flatten_and_batch_shift_indices(indices: torch.Tensor, sequence_length: int) -> torch.Tensor:
+    """
+    This is a subroutine for [`batched_index_select`](./util.md#batched_index_select).
+    The given `indices` of size `(batch_size, d_1, ..., d_n)` indexes into dimension 2 of a
+    target tensor, which has size `(batch_size, sequence_length, embedding_size)`. This
+    function returns a vector that correctly indexes into the flattened target. The sequence
+    length of the target must be provided to compute the appropriate offsets.
+
+    ```python
+        indices = torch.ones([2,3], dtype=torch.long)
+        # Sequence length of the target tensor.
+        sequence_length = 10
+        shifted_indices = flatten_and_batch_shift_indices(indices, sequence_length)
+        # Indices into the second element in the batch are correctly shifted
+        # to take into account that the target tensor will be flattened before
+        # the indices are applied.
+        assert shifted_indices == [1, 1, 1, 11, 11, 11]
+    ```
+
+    # Parameters
+
+    indices : `torch.LongTensor`, required.
+    sequence_length : `int`, required.
+        The length of the sequence the indices index into.
+        This must be the second dimension of the tensor.
+
+    # Returns
+
+    offset_indices : `torch.LongTensor`
+    """
+    # Shape: (batch_size)
+    if torch.max(indices) >= sequence_length or torch.min(indices) < 0:
+        raise ConfigurationError(
+            f"All elements in indices should be in range (0, {sequence_length - 1})"
+        )
+    offsets = get_range_vector(indices.size(0), get_device_of(indices)) * sequence_length
+    for _ in range(len(indices.size()) - 1):
+        offsets = offsets.unsqueeze(1)
+
+    # Shape: (batch_size, d_1, ..., d_n)
+    offset_indices = indices + offsets
+
+    # Shape: (batch_size * d_1 * ... * d_n)
+    offset_indices = offset_indices.view(-1)
+    return offset_indices
+
+
+
+def batched_index_select(
+    target: torch.Tensor,
+    indices: torch.LongTensor,
+    flattened_indices: Optional[torch.LongTensor] = None,
+) -> torch.Tensor:
+    """
+    The given `indices` of size `(batch_size, d_1, ..., d_n)` indexes into the sequence
+    dimension (dimension 2) of the target, which has size `(batch_size, sequence_length,
+    embedding_size)`.
+
+    This function returns selected values in the target with respect to the provided indices, which
+    have size `(batch_size, d_1, ..., d_n, embedding_size)`. This can use the optionally
+    precomputed `flattened_indices` with size `(batch_size * d_1 * ... * d_n)` if given.
+
+    An example use case of this function is looking up the start and end indices of spans in a
+    sequence tensor. This is used in the
+    [CoreferenceResolver](https://docs.allennlp.org/models/main/models/coref/models/coref/)
+    model to select contextual word representations corresponding to the start and end indices of
+    mentions.
+
+    The key reason this can't be done with basic torch functions is that we want to be able to use look-up
+    tensors with an arbitrary number of dimensions (for example, in the coref model, we don't know
+    a-priori how many spans we are looking up).
+
+    # Parameters
+
+    target : `torch.Tensor`, required.
+        A 3 dimensional tensor of shape (batch_size, sequence_length, embedding_size).
+        This is the tensor to be indexed.
+    indices : `torch.LongTensor`
+        A tensor of shape (batch_size, ...), where each element is an index into the
+        `sequence_length` dimension of the `target` tensor.
+    flattened_indices : `Optional[torch.Tensor]`, optional (default = `None`)
+        An optional tensor representing the result of calling `flatten_and_batch_shift_indices`
+        on `indices`. This is helpful in the case that the indices can be flattened once and
+        cached for many batch lookups.
+
+    # Returns
+
+    selected_targets : `torch.Tensor`
+        A tensor with shape [indices.size(), target.size(-1)] representing the embedded indices
+        extracted from the batch flattened target tensor.
+    """
+    if flattened_indices is None:
+        # Shape: (batch_size * d_1 * ... * d_n)
+        flattened_indices = flatten_and_batch_shift_indices(indices, target.size(1))
+
+    # Shape: (batch_size * sequence_length, embedding_size)
+    flattened_target = target.reshape(-1, target.size(-1))
+
+    # Shape: (batch_size * d_1 * ... * d_n, embedding_size)
+    flattened_selected = flattened_target.index_select(0, flattened_indices)
+    selected_shape = list(indices.size()) + [target.size(-1)]
+    # Shape: (batch_size, d_1, ..., d_n, embedding_size)
+    selected_targets = flattened_selected.view(*selected_shape)
+    return selected_targets
+
+
+
+class EndpointSpanExtractor(SpanExtractorWithSpanWidthEmbedding):
+    """
+    Represents spans as a combination of the embeddings of their endpoints. Additionally,
+    the width of the spans can be embedded and concatenated on to the final combination.
+
+    The following types of representation are supported, assuming that
+    `x = span_start_embeddings` and `y = span_end_embeddings`.
+
+    `x`, `y`, `x*y`, `x+y`, `x-y`, `x/y`, where each of those binary operations
+    is performed elementwise.  You can list as many combinations as you want, comma separated.
+    For example, you might give `x,y,x*y` as the `combination` parameter to this class.
+    The computed similarity function would then be `[x; y; x*y]`, which can then be optionally
+    concatenated with an embedded representation of the width of the span.
+
+    Registered as a `SpanExtractor` with name "endpoint".
+
+    # Parameters
+
+    input_dim : `int`, required.
+        The final dimension of the `sequence_tensor`.
+    combination : `str`, optional (default = `"x,y"`).
+        The method used to combine the `start_embedding` and `end_embedding`
+        representations. See above for a full description.
+    num_width_embeddings : `int`, optional (default = `None`).
+        Specifies the number of buckets to use when representing
+        span width features.
+    span_width_embedding_dim : `int`, optional (default = `None`).
+        The embedding size for the span_width features.
+    bucket_widths : `bool`, optional (default = `False`).
+        Whether to bucket the span widths into log-space buckets. If `False`,
+        the raw span widths are used.
+    use_exclusive_start_indices : `bool`, optional (default = `False`).
+        If `True`, the start indices extracted are converted to exclusive indices. Sentinels
+        are used to represent exclusive span indices for the elements in the first
+        position in the sequence (as the exclusive indices for these elements are outside
+        of the the sequence boundary) so that start indices can be exclusive.
+        NOTE: This option can be helpful to avoid the pathological case in which you
+        want span differences for length 1 spans - if you use inclusive indices, you
+        will end up with an `x - x` operation for length 1 spans, which is not good.
+    """
+
+    def __init__(
+        self,
+        input_dim: int,
+        combination: str = "x,y",
+        num_width_embeddings: int = None,
+        span_width_embedding_dim: int = None,
+        bucket_widths: bool = False,
+        use_exclusive_start_indices: bool = False,
+    ) -> None:
+        super().__init__(
+            input_dim=input_dim,
+            num_width_embeddings=num_width_embeddings,
+            span_width_embedding_dim=span_width_embedding_dim,
+            bucket_widths=bucket_widths,
+        )
+        self._combination = combination
+
+        self._use_exclusive_start_indices = use_exclusive_start_indices
+        if use_exclusive_start_indices:
+            self._start_sentinel = Parameter(torch.randn([1, 1, int(input_dim)]))
+
+    def get_output_dim(self) -> int:
+        combined_dim = util.get_combined_dim(self._combination, [self._input_dim, self._input_dim])
+        if self._span_width_embedding is not None:
+            return combined_dim + self._span_width_embedding.get_output_dim()
+        return combined_dim
+
+    def _embed_spans(
+        self,
+        sequence_tensor: torch.FloatTensor,
+        span_indices: torch.LongTensor,
+        sequence_mask: torch.BoolTensor = None,
+        span_indices_mask: torch.BoolTensor = None,
+    ) -> None:
+        # shape (batch_size, num_spans)
+        span_starts, span_ends = [index.squeeze(-1) for index in span_indices.split(1, dim=-1)]
+
+        if span_indices_mask is not None:
+            # It's not strictly necessary to multiply the span indices by the mask here,
+            # but it's possible that the span representation was padded with something other
+            # than 0 (such as -1, which would be an invalid index), so we do so anyway to
+            # be safe.
+            span_starts = span_starts * span_indices_mask
+            span_ends = span_ends * span_indices_mask
+
+        if not self._use_exclusive_start_indices:
+            if sequence_tensor.size(-1) != self._input_dim:
+                raise ValueError(
+                    f"Dimension mismatch expected ({sequence_tensor.size(-1)}) "
+                    f"received ({self._input_dim})."
+                )
+            start_embeddings = util.batched_index_select(sequence_tensor, span_starts)
+            end_embeddings = util.batched_index_select(sequence_tensor, span_ends)
+
+        else:
+            # We want `exclusive` span starts, so we remove 1 from the forward span starts
+            # as the AllenNLP `SpanField` is inclusive.
+            # shape (batch_size, num_spans)
+            exclusive_span_starts = span_starts - 1
+            # shape (batch_size, num_spans, 1)
+            start_sentinel_mask = (exclusive_span_starts == -1).unsqueeze(-1)
+            exclusive_span_starts = exclusive_span_starts * ~start_sentinel_mask.squeeze(-1)
+
+            # We'll check the indices here at runtime, because it's difficult to debug
+            # if this goes wrong and it's tricky to get right.
+            if (exclusive_span_starts < 0).any():
+                raise ValueError(
+                    f"Adjusted span indices must lie inside the the sequence tensor, "
+                    f"but found: exclusive_span_starts: {exclusive_span_starts}."
+                )
+
+            start_embeddings = util.batched_index_select(sequence_tensor, exclusive_span_starts)
+            end_embeddings = util.batched_index_select(sequence_tensor, span_ends)
+
+            # We're using sentinels, so we need to replace all the elements which were
+            # outside the dimensions of the sequence_tensor with the start sentinel.
+            start_embeddings = (
+                start_embeddings * ~start_sentinel_mask + start_sentinel_mask * self._start_sentinel
+            )
+
+        combined_tensors = util.combine_tensors(
+            self._combination, [start_embeddings, end_embeddings]
+        )
+
+        return combined_tensors
