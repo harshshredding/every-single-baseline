41411967cfafb2bc07b53414918ee6da1f6e650d
Harsh Verma <harshshredding@gmail.com>
2023-04-18 15:35:02 -0400

    try bigger batch size

--- experiments/experiment_ncbi_meta_special_super.py
+++ experiments/experiment_ncbi_meta_special_super.py
@@ -1,4 +1,4 @@
-from utils.config import get_experiment_config, Epochs30Modifier, AccuracyEvaluationModifier, TestEveryEpochModifier, AdafactorModifier, AdamModifier
+from utils.config import BiggerBatchModifier, get_experiment_config, Epochs30Modifier, AccuracyEvaluationModifier, TestEveryEpochModifier, AdafactorModifier, AdamModifier
 
 
 experiments = [
@@ -18,8 +18,30 @@ experiments = [
         modifiers=[
             Epochs30Modifier(),
             TestEveryEpochModifier(),
+            AdafactorModifier(),
+            AccuracyEvaluationModifier(),
+            BiggerBatchModifier()
+        ]
+    ),
+    get_experiment_config(
+        model_config_module_name='model_meta_special_tokens_bio',
+        dataset_config_name='ncbi_sentence_meta_super_special_gold',
+        modifiers=[
+            Epochs30Modifier(),
+            TestEveryEpochModifier(),
             AdamModifier(),
             AccuracyEvaluationModifier()
+        ]
+    ),
+    get_experiment_config(
+        model_config_module_name='model_meta_special_tokens_bio',
+        dataset_config_name='ncbi_sentence_meta_super_special_gold',
+        modifiers=[
+            Epochs30Modifier(),
+            TestEveryEpochModifier(),
+            AdamModifier(),
+            AccuracyEvaluationModifier(),
+            BiggerBatchModifier()
         ]
     ),
 ]
