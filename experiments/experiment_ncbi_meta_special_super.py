190242c3a6cc5ea1fef822e2c789319b048e08ae
Harsh Verma <harshshredding@gmail.com>
2023-04-18 15:44:58 -0400

    bug, swap configs

--- experiments/experiment_ncbi_meta_special_super.py
+++ experiments/experiment_ncbi_meta_special_super.py
@@ -1,10 +1,10 @@
-from utils.config import BiggerBatchModifier, get_experiment_config, Epochs30Modifier, AccuracyEvaluationModifier, TestEveryEpochModifier, AdafactorModifier, AdamModifier
+from utils.config import get_experiment_config, Epochs30Modifier, AccuracyEvaluationModifier, TestEveryEpochModifier, AdafactorModifier, AdamModifier
 
 
 experiments = [
     get_experiment_config(
         model_config_module_name='model_meta_special_tokens_bio',
-        dataset_config_name='ncbi_sentence_meta_super_special_gold',
+        dataset_config_name='ncbi_sentence_meta_super_special_tokens',
         modifiers=[
             Epochs30Modifier(),
             TestEveryEpochModifier(),
@@ -14,34 +14,12 @@ experiments = [
     ),
     get_experiment_config(
         model_config_module_name='model_meta_special_tokens_bio',
-        dataset_config_name='ncbi_sentence_meta_super_special_gold',
-        modifiers=[
-            Epochs30Modifier(),
-            TestEveryEpochModifier(),
-            AdafactorModifier(),
-            AccuracyEvaluationModifier(),
-            BiggerBatchModifier()
-        ]
-    ),
-    get_experiment_config(
-        model_config_module_name='model_meta_special_tokens_bio',
-        dataset_config_name='ncbi_sentence_meta_super_special_gold',
+        dataset_config_name='ncbi_sentence_meta_super_special_tokens',
         modifiers=[
             Epochs30Modifier(),
             TestEveryEpochModifier(),
             AdamModifier(),
             AccuracyEvaluationModifier()
-        ]
-    ),
-    get_experiment_config(
-        model_config_module_name='model_meta_special_tokens_bio',
-        dataset_config_name='ncbi_sentence_meta_super_special_gold',
-        modifiers=[
-            Epochs30Modifier(),
-            TestEveryEpochModifier(),
-            AdamModifier(),
-            AccuracyEvaluationModifier(),
-            BiggerBatchModifier()
         ]
     ),
 ]
