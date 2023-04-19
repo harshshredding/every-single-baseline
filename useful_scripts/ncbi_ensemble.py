a20136e911827b8767b270a6a6db98fec520e59f
Harsh Verma <harshshredding@gmail.com>
2023-04-19 16:22:23 -0400

    add union ensembling

--- useful_scripts/ncbi_ensemble.py
+++ useful_scripts/ncbi_ensemble.py
@@ -1,6 +1,6 @@
 from utils.evaluation import get_f1_score_from_sets, get_gold_annos_set
 from structs import DatasetSplit
-from utils.ensemble import get_majority_vote_predictions
+from utils.ensemble import get_majority_vote_predictions, get_union_predictions
 
 
 def majority_vote_f1_ncbi_several():
@@ -11,11 +11,11 @@ def majority_vote_f1_ncbi_several():
         test_prediction_file_paths.append(
             f'/Users/harshverma/every-single-baseline/meta/ncbi/predictions/test/experiment_ncbi_sentence_ncbi_disease_sentence_model_seq_large_bio_test_epoch_{i}_predictions.tsv'
         )
-
     for i in range(15, 19):
         test_prediction_file_paths.append(
             f'/Users/harshverma/every-single-baseline/meta/ncbi/predictions/test/experiment_ncbi_sentence_ncbi_disease_sentence_model_span_large_bio_default_test_epoch_{i}_predictions.tsv'
         )
+
     assert len(test_prediction_file_paths) == 8
     majority_prediction_counts = get_majority_vote_predictions(test_prediction_file_paths)
 
@@ -24,7 +24,7 @@ def majority_vote_f1_ncbi_several():
     print(get_f1_score_from_sets(gold_set=gold_predictions, predicted_set=majority_predictions))
 
 
-def majority_vote_f1_ncbi_special_models():
+def majority_vote_f1_ncbi_best_models():
     gold_predictions = get_gold_annos_set(dataset_config_name='ncbi_disease_sentence', split=DatasetSplit.test)
 
     seq_predictions_file_path = '/Users/harshverma/every-single-baseline/meta/ncbi/predictions/test/experiment_ncbi_sentence_ncbi_disease_sentence_model_seq_large_bio_test_epoch_10_predictions.tsv'
@@ -36,3 +36,17 @@ def majority_vote_f1_ncbi_special_models():
     majority_predictions = set([anno for anno in majority_prediction_counts])
     assert len(majority_predictions) == len(majority_prediction_counts)
     print(get_f1_score_from_sets(gold_set=gold_predictions, predicted_set=majority_predictions))
+
+
+def union_f1_ncbi_best_models():
+    gold_predictions = get_gold_annos_set(dataset_config_name='ncbi_disease_sentence', split=DatasetSplit.test)
+
+    seq_predictions_file_path = '/Users/harshverma/every-single-baseline/meta/ncbi/predictions/test/experiment_ncbi_sentence_ncbi_disease_sentence_model_seq_large_bio_test_epoch_10_predictions.tsv'
+    span_predictions_file_path = '/Users/harshverma/every-single-baseline/meta/ncbi/predictions/test/experiment_ncbi_sentence_ncbi_disease_sentence_model_span_large_bio_default_test_epoch_15_predictions.tsv'
+    test_prediction_file_paths = [seq_predictions_file_path, span_predictions_file_path]
+
+    union_predictions = get_union_predictions(test_prediction_file_paths)
+
+    print(get_f1_score_from_sets(gold_set=gold_predictions, predicted_set=union_predictions))
+
+
--- utils/ensemble.py
+++ utils/ensemble.py
@@ -2,16 +2,35 @@ from collections import defaultdict
 from utils.general import read_predictions_file
 from structs import SampleAnno
 
+
 def get_majority_vote_predictions(prediction_file_paths: list[str]):
+    """
+    Combine the predictions of agents using the majority voting strategy i.e 
+    only keep those predictions that the majority of agents voted 'yes' on.
+    """
+
     votes: defaultdict[SampleAnno, int] = defaultdict(lambda: 0)
 
     for prediction_file_path in prediction_file_paths:
         predictions = read_predictions_file(prediction_file_path)
         for sample_id, annos in predictions.items():
             for anno in annos:
-                votes[SampleAnno(sample_id, 'Disease', anno.begin_offset, anno.end_offset)] += 1
+                votes[SampleAnno(sample_id, anno.label_type, anno.begin_offset, anno.end_offset)] += 1
 
     test_annos_majority_votes = {anno: count for anno, count in votes.items() if count > len(prediction_file_paths)//2 }
 
     return test_annos_majority_votes
+
+
+
+def get_union_predictions(prediction_file_paths: list[str]) -> set[SampleAnno]:
+    union_predictions: set[SampleAnno] = set()
+
+    for prediction_file_path in prediction_file_paths:
+        predictions = read_predictions_file(prediction_file_path)
+        for sample_id, annos in predictions.items():
+            for anno in annos:
+                union_predictions.add(SampleAnno(sample_id, anno.label_type, anno.begin_offset, anno.end_offset))
+
+    return union_predictions
 
