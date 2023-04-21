36558419ad1088bed684ec215e1f46e53c9edd33
Harsh Verma <harshshredding@gmail.com>
2023-04-21 09:23:32 -0400

    multiconer adafactor experiment

---
+++ experiments/experiment_multiconer_adafactor.py
@@ -0,0 +1,45 @@
+from utils.config import AdafactorModifier, AdamModifier, TinySpanWidthModifier, get_experiment_config, Epochs20Modifier, BiggerBatchModifier, TestEveryEpochModifier, SmallerSpanWidthModifier
+
+
+experiments = [
+    get_experiment_config(
+        model_config_module_name='model_span_large_default',
+        dataset_config_name='multiconer_fine_vanilla',
+        modifiers=[
+            Epochs20Modifier(),
+            TestEveryEpochModifier(),
+            SmallerSpanWidthModifier(),
+            AdafactorModifier()
+        ]
+    ),
+    get_experiment_config(
+        model_config_module_name='model_seq_large_default',
+        dataset_config_name='multiconer_fine_vanilla',
+        modifiers=[
+            Epochs20Modifier(),
+            TestEveryEpochModifier(),
+            SmallerSpanWidthModifier(),
+            AdafactorModifier()
+        ]
+    ),
+    get_experiment_config(
+        model_config_module_name='model_span_large_default',
+        dataset_config_name='multiconer_fine_vanilla',
+        modifiers=[
+            Epochs20Modifier(),
+            TestEveryEpochModifier(),
+            SmallerSpanWidthModifier(),
+            AdamModifier()
+        ]
+    ),
+    get_experiment_config(
+        model_config_module_name='model_seq_large_default',
+        dataset_config_name='multiconer_fine_vanilla',
+        modifiers=[
+            Epochs20Modifier(),
+            TestEveryEpochModifier(),
+            SmallerSpanWidthModifier(),
+            AdamModifier()
+        ]
+    ),
+]
