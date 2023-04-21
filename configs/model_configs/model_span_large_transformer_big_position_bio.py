24dce43a5dad1538eda9d377e77ba0f71af95c20
Harsh Verma <harshshredding@gmail.com>
2023-04-21 08:25:05 -0400

    fix model config bug

--- configs/model_configs/model_span_large_transformer_big_position_bio.py
+++ configs/model_configs/model_span_large_transformer_big_position_bio.py
@@ -1,8 +1,8 @@
 from utils.config import ModelConfig, get_large_model_config_bio
 
-def create_model_config():
+def create_model_config(model_config_name: str):
     model_config: ModelConfig = get_large_model_config_bio(
-        model_config_name='span_large_default',
+        model_config_name=model_config_name,
         model_name='SpanDefaultTransformerBiggerPosition'
     )
     return model_config
